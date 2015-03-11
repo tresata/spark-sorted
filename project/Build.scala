0d99d6278b02d17bc166185c15e3613f6827953d
Koert Kuipers <koert@tresata.com>
2015-03-11 18:49:13 -0400

    publish to sonatype OSSRH

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,36 @@ object ProjectBuild extends Build {
       ),
       publishMavenStyle := true,
       pomIncludeRepository := { x => false },
-      publishArtifact in Test := false
+      publishArtifact in Test := false,
+      publishTo := {
+        val nexus = "https://oss.sonatype.org/"
+        if (isSnapshot.value)
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+      },
+      credentials += Credentials(Path.userHome / ".m2" / "credentials_sonatype"),
+      pomExtra := (
+        <url>https://github.com/tresata/spark-scalding</url>
+        <licenses>
+          <license>
+            <name>Apache 2</name>
+            <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>  
+            <distribution>repo</distribution>
+            <comments>A business-friendly OSS license</comments>
+          </license>
+        </licenses>
+        <scm>
+          <url>git@github.com:tresata/spark-scalding.git</url>
+          <connection>scm:git:git@github.com:tresata/spark-scalding.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>koertkuipers</id>
+            <name>Koert Kuipers</name>
+            <url>https://github.com/koertkuipers</url>
+          </developer>
+        </developers>)
     )
   )
 }
