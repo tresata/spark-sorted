7a5173008f45a7829e3f6ef614add36fd5da0f33
Koert Kuipers <koert@tresata.com>
2015-08-04 15:10:02 -0400

    bugfix for issue 5: all keys within partition get dropped after key with empty iterator

--- src/test/scala/com/tresata/spark/sorted/GroupSortedSpec.scala
+++ src/test/scala/com/tresata/spark/sorted/GroupSortedSpec.scala
@@ -12,21 +12,21 @@ class GroupSortedSpec extends FunSpec {
   import PairRDDFunctions._
 
   describe("PairRDDFunctions") {
-    it("should do groupSort on a RDD without value ordering") {
+    it("should groupSort on a RDD without value ordering") {
       val rdd = sc.parallelize(List((1, 2), (2, 3), (1, 3), (3, 1), (2, 1)))
       val groupSorted = rdd.groupSort(new HashPartitioner(2), None)
       assert(groupSorted.map(_._1).collect === List(2, 2, 1, 1, 3)) // ordering depends on hashcode!
       assert(groupSorted.glom.collect.map(_.map(_._1).toList).toSet === Set(List(2, 2), List(1, 1, 3)))
     }
 
-    it("should do groupSort on a RDD with value ordering") {
+    it("should groupSort on a RDD with value ordering") {
       val rdd = sc.parallelize(List((1, 2), (2, 3), (1, 3), (3, 1), (2, 1)))
       val groupSorted = rdd.groupSort(new HashPartitioner(2), Some(implicitly[Ordering[Int]]))
       assert(groupSorted.map(_._1).collect === List(2, 2, 1, 1, 3)) // ordering depends on hashcode!
       assert(groupSorted.glom.collect.map(_.toList).toSet ===  Set(List((2, 1), (2, 3)), List((1, 2), (1, 3), (3, 1))))
     }
 
-    it("should do groupSort on a GroupSorted with no effect") {
+    it("should groupSort on a GroupSorted with no effect") {
       val rdd = sc.parallelize(List((1, 2), (2, 3), (1, 3), (3, 1), (2, 1)))
       val groupSorted = rdd.groupSort(new HashPartitioner(2), Some(implicitly[Ordering[Int]]))
       assert(groupSorted.groupSort(new HashPartitioner(2), Some(implicitly[Ordering[Int]])) eq groupSorted)
@@ -34,13 +34,13 @@ class GroupSortedSpec extends FunSpec {
   }
 
   describe("GroupSorted") {
-    it("should do mapStreamByKey without value ordering") {
+    it("should mapStreamByKey without value ordering") {
       val rdd = sc.parallelize(List(("a", 1), ("b", 10), ("a", 3), ("b", 1), ("c", 5)))
       val sets = rdd.groupSort(new HashPartitioner(2), None).mapStreamByKey(iter => Iterator(iter.toSet)) // very contrived...
       assert(sets.collect.toMap ===  Map("a" -> Set(1, 3), "b" -> Set(1, 10), "c" -> Set(5)))
     }
 
-    it("should do mapStreamByKey with value ordering") {
+    it("should mapStreamByKey with value ordering") {
       val rdd = sc.parallelize(List(("a", 1), ("b", 10), ("a", 3), ("b", 1), ("c", 5)))
       val withMax = rdd.groupSort(new HashPartitioner(2), Some(implicitly[Ordering[Int]].reverse)).mapStreamByKey{ iter =>
         val buffered = iter.buffered
@@ -50,13 +50,13 @@ class GroupSortedSpec extends FunSpec {
       assert(withMax.collect.toList.groupBy(identity).mapValues(_.size) ===  Map(("a", 3) -> 2, ("b", 10) -> 2, ("c", 5) -> 1))
     }
 
-    it("should do foldLeftByKey without value ordering") {
+    it("should foldLeftByKey without value ordering") {
       val rdd = sc.parallelize(List(("c", "x"), ("a", "b"), ("a", "c"), ("b", "e"), ("b", "d")))
       val sets = rdd.groupSort(new HashPartitioner(2), None).foldLeftByKey(Set.empty[String]){ case (set, str) => set + str }
       assert(sets.collect.toMap === Map("a" -> Set("b", "c"), "b" -> Set("d", "e"), "c" -> Set("x")))
     }
 
-    it("should do foldLeftByKey with value ordering") {
+    it("should foldLeftByKey with value ordering") {
       val ord = Ordering.by[TimeValue, Int](_.time)
       val tseries = sc.parallelize(List(
         (5, TimeValue(2, 0.5)), (1, TimeValue(1, 1.2)), (5, TimeValue(1, 1.0)),
@@ -66,7 +66,7 @@ class GroupSortedSpec extends FunSpec {
       assert(emas.collect.toSet === Set((1, 1.0736), (5, 0.26)))
     }
 
-    it("should do mapStreamByKey with value ordering while not exhausting iterators") {
+    it("should mapStreamByKey with value ordering while not exhausting iterators") {
       val rdd = sc.parallelize(List(("a", 1), ("b", 10), ("a", 3), ("b", 1), ("c", 5)))
       val withMax = rdd.groupSort(new HashPartitioner(2), Some(implicitly[Ordering[Int]].reverse)).mapStreamByKey{ iter =>
         Iterator(iter.next())
@@ -74,7 +74,14 @@ class GroupSortedSpec extends FunSpec {
       assert(withMax.collect.toSet ===  Set(("a", 3), ("b", 10), ("c", 5)))
     }
 
-    it("should do foldLeftByKey with a mutable accumulator") {
+    it("should mapStreamByKey if some keys have no output") {
+      // see https://github.com/tresata/spark-sorted/issues/5
+      val rdd = sc.parallelize(List(("a", 1), ("c", 10), ("a", 3), ("c", 1), ("b", 5)))
+      val filtered = rdd.groupSort(new HashPartitioner(1), Some(implicitly[Ordering[Int]].reverse)).mapStreamByKey(_.filter(_ < 5))
+      assert(filtered.collect.toSet ===  Set(("a", 1), ("a", 3), ("c", 1)))
+    }
+
+    it("should foldLeftByKey with a mutable accumulator") {
       import scala.collection.mutable.HashSet
       val rdd = sc.parallelize(List(("c", "x"), ("a", "b"), ("a", "c"), ("b", "e"), ("b", "d")))
       val sets = rdd.groupSort(new HashPartitioner(2), None).foldLeftByKey(new HashSet[String]){ case (set, str) => set.add(str); set }
