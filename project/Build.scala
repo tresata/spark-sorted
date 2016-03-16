90cf8f326dda47992c88206819c0cc0ef653560b
Koert Kuipers <koert@tresata.com>
2016-03-15 21:41:14 -0400

    update build and bump spark to 1.6.1

--- project/Build.scala
+++ project/Build.scala
@@ -1,13 +1,13 @@
 import sbt._
 import sbt.Keys._
 import sbtsparkpackage.SparkPackagePlugin.autoImport._
-import net.virtualvoid.sbt.graph.Plugin._
 
 object ProjectBuild extends Build {
   lazy val project = Project(
     id = "root",
-    base = file("."),
-    settings = Project.defaultSettings ++ graphSettings ++ Seq(
+    base = file(".")
+  ).settings(
+    Seq(
       organization := "com.tresata",
       name := "spark-sorted",
       version := "0.6.0-SNAPSHOT",
@@ -17,14 +17,14 @@ object ProjectBuild extends Build {
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-target:jvm-1.7", "-feature", "-language:_"),
       // sbt-spark-package settings
       spName := "tresata/spark-sorted",
-      sparkVersion := "1.6.0",
+      sparkVersion := "1.6.1",
       sparkComponents += "core",
       spIncludeMaven := true,
       spAppendScalaVersion := true,
       // end sbt-spark-package settings
       licenses += "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0.txt"),
-      libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.5" % "test",
-      libraryDependencies += "com.novocode" % "junit-interface" % "0.10" % "test",
+      libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test",
+      libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test",
       libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.4" % "test",
       testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-oF"),
       testOptions += Tests.Argument(TestFrameworks.JUnit, "-v", "-a"),
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,7 @@
-addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.5")
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.8.2")
 
 addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
 
 resolvers += "Spark Packages repo" at "https://dl.bintray.com/spark-packages/maven/"
 
-addSbtPlugin("org.spark-packages" %% "sbt-spark-package" % "0.2.1")
+addSbtPlugin("org.spark-packages" %% "sbt-spark-package" % "0.2.3")
