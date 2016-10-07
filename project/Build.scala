1cc867d4933dc2abdd69557f844cf0e030bf9c98
Koert Kuipers <koert@tresata.com>
2016-10-07 15:49:34 -0400

    release 0.9.0

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object ProjectBuild extends Build {
     Seq(
       organization := "com.tresata",
       name := "spark-sorted",
-      version := "0.9.0-SNAPSHOT",
+      version := "0.9.0",
       scalaVersion := "2.11.8",
       crossScalaVersions := Seq("2.10.6", "2.11.8"),
       javacOptions ++= Seq("-Xlint:unchecked", "-source", "1.7", "-target", "1.7"),
